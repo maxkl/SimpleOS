e4ec5f397d5c2ab14bf591c4718b03119ca7b968
maxkl <max@maxkl.de>
2016-10-12 16:42:01 +0200

    Lesson 04

--- src/boot_sect_simple.asm
+++ src/boot_sect_simple.asm
@@ -1,11 +1,33 @@
-; Base address of the boot sector
-[org 0x7c00]
-
 ; TTY mode
 mov ah, 0x0e
 
-; Print what resides at the label
-mov al, [the_secret]
+; Init stack at 0x8000
+mov bp, 0x8000
+mov sp, bp
+
+push 'A'
+push 'B'
+push 'C'
+
+mov al, [0x7ffe] ; 0x8000 - 2 = 0x7ffe
+int 0x10
+
+mov al, [0x8000]
+int 0x10
+
+pop bx
+mov al, bl
+int 0x10
+
+pop bx
+mov al, bl
+int 0x10
+
+pop bx
+mov al, bl
+int 0x10
+
+mov al, [0x8000]
 int 0x10
 
 jmp $ ; infinite loop
