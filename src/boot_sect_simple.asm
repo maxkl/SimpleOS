2891700f94d3f22f25f42ccb78090570b0c190ba
maxkl <max@maxkl.de>
2016-10-12 11:57:52 +0200

    Lesson 02

--- src/boot_sect_simple.asm
+++ src/boot_sect_simple.asm
@@ -1,9 +1,21 @@
-; Infinite loop (E9 FD FF)
-loop:
-	jmp loop
+; TTY mode
+mov ah, 0x0e
+; Character to print
+mov al, 'A'
+; General interrupt for video services
+int 0x10
+mov al, 's'
+int 0x10
+mov al, 'd'
+int 0x10
+mov al, 'f'
+int 0x10
+
+; Jump to the current address (infinite loop)
+jmp $
 
 ; Fill with 510 times 00 minus the prevoius code
-times 510-($-$$) db 0
+times 510 - ($-$$) db 0
 
 ; Magic number
-dw 0xAA55
+dw 0xaa55
